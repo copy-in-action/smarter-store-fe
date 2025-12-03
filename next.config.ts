e0dbb3d8a4b0d1a2eaf1803e52f016138192df6c
Choi HongCheol <choisieun89@gmail.com>
2025-12-03 18:30:55 +0900

    ci: github action 적용
    - Dockerfile추가
    - nextjs 빌드 설정 변경

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: process.platform === "win32" ? undefined : "standalone",
 };
 
 export default nextConfig;
