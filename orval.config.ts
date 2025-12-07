b824986ba402d86075b64e3256c898efe899ee09
Choi HongCheol <choisieun89@gmail.com>
2025-12-07 19:57:23 +0900

    feat: orval 설정 변경

--- orval.config.ts
+++ orval.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   petstore: {
     input: `${process.env.API_SERVER}/v3/api-docs`,
     output: {
-      mode: "split",
+      baseUrl: process.env.API_SERVER,
+      mode: "tags-split",
       target: "./src/shared/api/generated",
       client: "react-query",
       httpClient: "fetch",
@@ -21,6 +22,7 @@ export default defineConfig({
       target: `${process.env.API_SERVER}/v3/api-docs`,
     },
     output: {
+      baseUrl: process.env.API_SERVER,
       mode: "tags-split",
       client: "zod",
       target: "./src/shared/api/generated/schemas",
