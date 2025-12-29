20f5f63992f9d0f51bc5dd5fe096f336fc66bef0
Choi HongCheol <choisieun89@gmail.com>
2025-12-30 01:23:38 +0900

    fix: 메인페이지 스타일 오류, 하이드레이션 오류 수정

--- src/features/service/home/index.ts
+++ src/features/service/home/index.ts
@@ -3,5 +3,4 @@ export { banners, categories } from "./model";
 export { default as MainBanner } from "./ui/MainBanner";
 export { default as PerformanceCategory } from "./ui/PerformanceCategory";
 export { PerformanceList } from "./ui/PerformanceListClient";
-export { PerformanceListSkeleton } from "./ui/PerformanceListSkeleton";
 export { default as PerformanceListServer } from "./ui/PerformanceListServer";
